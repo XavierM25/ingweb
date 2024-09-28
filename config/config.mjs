0959c3b2ff0a9517b951d54f08c8b9de6287b901
zentoo31 <zentoo31@gmail.com>
2024-09-27 19:05:30 -0500

    Add mail module

--- config/config.mjs
+++ config/config.mjs
@@ -8,5 +8,6 @@ const DB_MONGO = process.env.DB_MONGO;
 const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS.split(',');
 const SALT_ROUNDS = process.env.SALT_ROUNDS;
 const SECRET_KEY = process.env.SECRET_KEY;
+const MAILGUN_API_KEY = process.env.MAILGUN_API_KEY;
 
-export { PORT, DB_MONGO, ALLOWED_ORIGINS, SALT_ROUNDS, SECRET_KEY };
+export { PORT, DB_MONGO, ALLOWED_ORIGINS, SALT_ROUNDS, SECRET_KEY, MAILGUN_API_KEY };
