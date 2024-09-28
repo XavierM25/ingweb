361ce36a5f21f3b6c26d16c33e1995b94f27e9cd
zentoo31 <zentoo31@gmail.com>
2024-09-27 21:44:21 -0500

    Fix enum type

--- schemas/userSchema.mjs
+++ schemas/userSchema.mjs
@@ -16,7 +16,7 @@ const UserSchema = new mongoose.Schema({
     },
     role:{
         type: String,
-        enum: ['teacher', 'student'],
+        enum: ['teacher', 'student', 'administrator'],
         default: 'student',
         required: true
     },
