a99101017566f1dce9d94be996b2f8a48137e59a
zentoo31 <zentoo31@gmail.com>
2024-09-30 07:45:07 -0500

    30/09/24

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -4,5 +4,6 @@ import { UserController } from "../controllers/userController.mjs";
 const userRouter = e.Router();
 
 userRouter.get('/get-info', UserController.getUser);
+userRouter.post('/set-username', UserController.setUsername)
 
 export default userRouter;
